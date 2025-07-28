eacd14e77f6a963ce80fa2418136f079fcf2e1fa
syed-amin <syedamin000000@gmail.com>
2025-07-28 12:48:02 +0600

    refactor: remove redundant text from login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -71,9 +71,7 @@ const Login = () => {
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-sage-200"></div>
               </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-sage-500">Or continue with email</span>
-              </div>
+             
             </div>
 
             {/* Login Form */}

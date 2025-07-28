95a05f79dc0d7b4f88caccd0e495ee23b556b5f8
nadia741 <nadiachowdhury741@gmail.com>
2025-07-28 12:49:53 +0600

    divider removed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -76,15 +76,6 @@ const Login = () => {
               </Button>
             </div>
 
-            {/* Divider */}
-            <div className="relative mb-6">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-sage-200"></div>
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-sage-500">Or continue with email</span>
-              </div>
-            </div>
 
             {/* Login Form */}
             <form onSubmit={handleSubmit} className="space-y-6">
